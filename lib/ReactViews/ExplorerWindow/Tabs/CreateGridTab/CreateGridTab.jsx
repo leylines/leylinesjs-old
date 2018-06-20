59cfdbbb0b05313cc111ad3af4f70314c16e20b9
Joerg Roth <info@leylines.ch>
2018-06-20 09:31:48 +0200

    import propTypes

--- lib/ReactViews/ExplorerWindow/Tabs/CreateGridTab/CreateGridTab.jsx
+++ lib/ReactViews/ExplorerWindow/Tabs/CreateGridTab/CreateGridTab.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import DataCatalogGroup from '../../../DataCatalog/DataCatalogGroup.jsx';
 import DataPreview from '../../../Preview/DataPreview.jsx';
 import AddGridData from './AddGridData.jsx';
